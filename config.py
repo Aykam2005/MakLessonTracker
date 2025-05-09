944476eeca638fb836a0d8e59ffeedd1c3e40008
Aykam2005 <aykambertrand@gmail.com>
2025-05-09 13:41:23 -0400

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 CLIENT_ID = os.environ.get("MS_GRAPH_CLIENT_ID")
 CLIENT_SECRET = os.environ.get("MS_GRAPH_CLIENT_SECRET")
-TENANT_ID = os.environ.get("MS_GRAPH_TENANT_ID")
+TENANT_ID = "your-actual-tenant-id"  # <- this should be a valid GUID like '8f46243b-217e-4fe7-bd5c-a97939a6d86d'
 AUTHORITY = f"https://login.microsoftonline.com/{TENANT_ID}"
 REDIRECT_URI = "http://localhost:5000/callback"
 SCOPE = ["Files.ReadWrite.All", "User.Read"]
