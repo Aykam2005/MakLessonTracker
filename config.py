1eb71b2afc7a08433c2948537b26c092df13e51f
Aykam2005 <aykambertrand@gmail.com>
2025-05-09 13:52:14 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
-
 import os
 
-CLIENT_ID = os.environ.get("MS_GRAPH_CLIENT_ID")
-CLIENT_SECRET = os.environ.get("MS_GRAPH_CLIENT_SECRET")
-TENANT_ID = "your-actual-tenant-id"  # <- this should be a valid GUID like '8f46243b-217e-4fe7-bd5c-a97939a6d86d'
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+TENANT_ID = os.getenv("TENANT_ID")
 AUTHORITY = f"https://login.microsoftonline.com/{TENANT_ID}"
-REDIRECT_URI = "http://localhost:5000/callback"
-SCOPE = ["Files.ReadWrite.All", "User.Read"]
+REDIRECT_URI = os.getenv("REDIRECT_URI")
+SCOPE = ["User.Read", "Files.ReadWrite.All", "offline_access"]
